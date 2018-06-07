464c60177e49d7cc9ab5aa1f9354a3530b8f2a7b
Anton Oparin <mavarazy@gmail.com>
2018-06-07 21:52:40 +0300

    Making launchable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { composeWithDevTools } from "redux-devtools-extension";
+import promiseMiddleware from "redux-promise";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(promiseMiddleware, thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
