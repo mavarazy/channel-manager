16dd1bae67b23ab1985705f2b9629bd1e6d2873e
Anton Oparin <mavarazy@gmail.com>
2018-06-14 16:20:34 +0300

    Adding icons to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,15 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import {
+  faBookmark,
+  faCalendar,
+  faChartLine,
+  faHome,
+  faUserCircle,
+  faUserCog,
+  faSignOutAlt
+} from '@fortawesome/fontawesome-free-solid'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faUserCircle } from '@fortawesome/fontawesome-free-solid'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navigation = () => (
   <nav className="navbar is-spaced" aria-label="main navigation">
@@ -15,17 +23,35 @@ export const Navigation = () => (
 
     <div className="navbar-menu">
       <div className="navbar-start">
-        <NavLink className="navbar-item" activeClassName="is-active" to="/bookings">
-          Bookings
+        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/bookings">
+          <span className="icon">
+            <FontAwesomeIcon icon={faBookmark}/>
+          </span>
+          <span>
+            Bookings
+          </span>
         </NavLink>
-        <NavLink className="navbar-item" activeClassName="is-active" to="/calendar">
-          Calendar
+        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/calendar">
+          <span className="icon">
+            <FontAwesomeIcon icon={faCalendar}/>
+          </span>
+          <span>Calendar</span>
         </NavLink>
-        <NavLink className="navbar-item" activeClassName="is-active" to="/reports">
-          Reports
+        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/reports">
+          <span className="icon">
+            <FontAwesomeIcon icon={faChartLine}/>
+          </span>
+          <span>
+            Reports
+          </span>
         </NavLink>
-        <NavLink className="navbar-item" activeClassName="is-active" to="/listings">
-          Listings
+        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/listings">
+          <span className="icon">
+            <FontAwesomeIcon icon={faHome}/>
+          </span>
+          <span>
+            Listings
+          </span>
         </NavLink>
       </div>
 
@@ -36,11 +62,21 @@ export const Navigation = () => (
           </div>
           <div className="navbar-dropdown is-boxed is-right">
             <NavLink className="navbar-item" activeClassName="is-active" to="/profile">
-              Profile
+              <span className="icon">
+                <FontAwesomeIcon icon={faUserCog}/>
+              </span>
+              <span>
+                Profile
+              </span>
             </NavLink>
             <hr className="navbar-divider"/>
             <Link className="navbar-item" to="/auth/logout">
-              Logout
+              <span className="icon">
+                <FontAwesomeIcon icon={faSignOutAlt}/>
+              </span>
+              <span>
+                Logout
+              </span>
             </Link>
           </div>
         </div>
