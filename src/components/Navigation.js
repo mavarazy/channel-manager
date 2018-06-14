1e1dae6fb3073a991b3709af9f3ad32be16d6784
Anton Oparin <mavarazy@gmail.com>
2018-06-14 20:16:31 +0300

    Making Navigation mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,66 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { BookingsIcon, CalendarIcon, ReportsIcon, ListingsIcon, ProfileIcon, LogoutIcon, UserIcon } from "./icon";
+import { BookingsIcon, CalendarIcon, ListingsIcon, LogoutIcon, ProfileIcon, ReportsIcon, UserIcon } from "./icon";
+import cx from "classnames";
 
-export const Navigation = () => (
-  <nav className="navbar is-spaced" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"/>
-        <span aria-hidden="true"/>
-        <span aria-hidden="true"/>
-      </a>
-    </div>
+export class Navigation extends Component {
+  state = { isActive: false };
 
-    <div className="navbar-menu">
-      <div className="navbar-start">
-        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/bookings">
-          <BookingsIcon/>
-          <span>
+  toggleActive = () => this.setState(({ isActive }) => ({ isActive: !isActive }));
+
+  render() {
+    return (
+      <nav className="navbar is-spaced" aria-label="main navigation">
+        <div className="navbar-brand" onClick={this.toggleActive}>
+          <a role="button" className={cx("navbar-burger", { "is-active": this.state.isActive })} aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+          </a>
+        </div>
+
+        <div className={cx("navbar-menu", { "is-active": this.state.isActive })} onClick={this.toggleActive}>
+          <div className="navbar-start">
+            <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/bookings">
+              <BookingsIcon/>
+              <span>
             Bookings
           </span>
-        </NavLink>
-        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/calendar">
-          <CalendarIcon/>
-          <span>Calendar</span>
-        </NavLink>
-        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/reports">
-          <ReportsIcon/>
-          <span>
-            Reports
-          </span>
-        </NavLink>
-        <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/listings">
-          <ListingsIcon/>
-          <span>
-            Listings
-          </span>
-        </NavLink>
-      </div>
-
-      <div className="navbar-end">
-        <div className="navbar-item has-dropdown is-hoverable">
-          <div className="navbar-link">
-            <UserIcon size="2x"/>
-          </div>
-          <div className="navbar-dropdown is-boxed is-right">
-            <NavLink className="navbar-item" activeClassName="is-active" to="/profile">
-              <ProfileIcon/>
-              <span>
-                Profile
-              </span>
             </NavLink>
-            <hr className="navbar-divider"/>
-            <Link className="navbar-item" to="/auth/logout">
+            <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/calendar">
+              <CalendarIcon/>
+              <span>Calendar</span>
+            </NavLink>
+            <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/reports">
+              <ReportsIcon/>
+              <span>Reports</span>
+            </NavLink>
+            <NavLink className="navbar-item" activeClassName="is-active has-text-link" to="/listings">
+              <ListingsIcon/>
+              <span>Listings</span>
+            </NavLink>
+            <NavLink className="navbar-item is-hidden-desktop is-hidden-tablet" activeClassName="is-active" to="/profile">
+              <ProfileIcon/>
+              <span>Profile</span>
+            </NavLink>
+            <Link className="navbar-item is-hidden-desktop is-hidden-tablet" to="/auth/logout">
               <LogoutIcon/>
-              <span>
-                Logout
-              </span>
+              <span>Logout</span>
             </Link>
           </div>
-        </div>
-      </div>
 
-    </div>
-  </nav>
-);
+          <div className="navbar-end is-hidden-mobile">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <div className="navbar-link">
+                <UserIcon size="2x"/>
+              </div>
+              <div className="navbar-dropdown is-boxed is-right">
+                <NavLink className="navbar-item" activeClassName="is-active" to="/profile">
+                  <ProfileIcon/>
+                  <span>Profile</span>
+                </NavLink>
+                <hr className="navbar-divider"/>
+                <Link className="navbar-item" to="/auth/logout">
+                  <LogoutIcon/>
+                  <span>Logout</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </nav>
+    );
+  }
+}
--- src/components/icon/ReportsIcon.js
+++ src/components/icon/ReportsIcon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { faChartLine } from '@fortawesome/fontawesome-free-solid'
+import { faChartPie } from '@fortawesome/fontawesome-free-solid'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
-export const ReportsIcon = () => (<span className="icon"><FontAwesomeIcon icon={faChartLine}/></span>);
+export const ReportsIcon = () => (<span className="icon"><FontAwesomeIcon icon={faChartPie}/></span>);
