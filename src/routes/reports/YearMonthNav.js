1124f805e3a9c5f8cf3c80ca8cf325e2c6f478d6
Anton Oparin <mavarazy@gmail.com>
2018-06-14 17:49:02 +0300

    Updating navigation to use tabs instead of buttons

--- src/routes/reports/YearMonthNav.js
+++ src/routes/reports/YearMonthNav.js
@@ -1,73 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import ListNavigation from "../../components/ListNavigation";
 
 const YearMonthNav = ({ match: { params: { year, listingId }}}) => (
-  <div className="field has-addons has-text-centered">
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}`} exact>
-        {year}
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/1`}>
-        Jan
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/2`}>
-        Feb
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/3`}>
-        Mar
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/4`}>
-        Apr
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/5`}>
-        May
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/6`}>
-        Jun
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/7`}>
-        Jul
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/8`}>
-        Aug
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/9`}>
-        Sep
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/10`}>
-        Oct
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/11`}>
-        Nov
-      </NavLink>
-    </p>
-    <p className="control">
-      <NavLink activeClassName="is-primary" className="button" to={`/reports/${listingId}/${year}/12`}>
-        Dec
-      </NavLink>
-    </p>
+  <div className="tabs is-toggle">
+    <ListNavigation routes={
+      [
+        { to: `/reports/${listingId}/${year}`, text: year, exact: true },
+        { to: `/reports/${listingId}/${year}/1`, text: "Jan" },
+        { to: `/reports/${listingId}/${year}/2`, text: "Feb" },
+        { to: `/reports/${listingId}/${year}/3`, text: "Mar" },
+        { to: `/reports/${listingId}/${year}/4`, text: "Apr" },
+        { to: `/reports/${listingId}/${year}/5`, text: "May" },
+        { to: `/reports/${listingId}/${year}/6`, text: "Jun" },
+        { to: `/reports/${listingId}/${year}/7`, text: "Jul" },
+        { to: `/reports/${listingId}/${year}/8`, text: "Aug" },
+        { to: `/reports/${listingId}/${year}/9`, text: "Sep" },
+        { to: `/reports/${listingId}/${year}/10`, text: "Oct" },
+        { to: `/reports/${listingId}/${year}/11`, text: "Nov" },
+        { to: `/reports/${listingId}/${year}/12`, text: "Dec" },
+      ]
+    }/>
   </div>
 );
 
